5320dfa63ff3c60f056d13d49efafbdb6180e307
Ariel Parra <darkaico@gmail.com>
2019-09-21 12:42:11 -0300

    Ejercicios hechos en casa

--- flask_example/app.py
+++ flask_example/app.py
@@ -1,20 +1,54 @@
-from flask import Flask
-app = Flask(__name__)
+import json
 
+from flask import (
+  Flask,
+  render_template,
+  send_from_directory,
+)
 
-@app.route("/")
-def hello():
-    return "Un cambio aca!"
+app = Flask(__name__, static_folder = 'templates')
+
+@app.route('/')
+def index():
+    return send_from_directory('templates', "index.html")
+
+@app.route('/<path:path>')
+def hello(path):
+    return send_from_directory('templates', path)
+
 
 
 """
-@api {get} /hola/<nombre> Saludar al usuario
+@api {get} /customers/<id> Obtener Cliente
+@apiName GetCustomer
+@apiGroup Customer
 
-@apiParam {nombre} Nombre del usuario
+@apiParam {id} id del cliente
+
+@apiSuccess {String} id Id del cliente.
+@apiSuccess {String} name Nombre del cliente.
 """
-@app.route("/hola/<nombre>")
-def hello_name():
-    return "Hola !" + nombre
+@app.route("/v1/customers/<id>")
+def get_customer(id):
+    return json.dumps({
+        "id": id,
+        "name": "Ariel"
+    })
+
+"""
+@api {delete} /customers/<id> Elimina un Cliente
+@apiName DeleteCustomer
+@apiGroup Customer
+
+@apiParam {id} id del cliente
+
+@apiSuccess {Boolean} success Resultado de la operacion
+"""
+@app.route("/v1/customers/<id>", methods=['DELETE'])
+def delete_customer(id):
+    return json.dumps({
+        "success": True
+    })
 
 
 if __name__ == '__main__':
